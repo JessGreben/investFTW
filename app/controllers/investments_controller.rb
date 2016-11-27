9229bc9a7fa64b364fb9285e6c7acb34ec594179
Jessica Grebenschikov <Jessica@bright.md>
2016-11-27 13:59:43 -0800

    add quandle key to api call

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,6 +1,7 @@
 class InvestmentsController < ApplicationController
 	def new
-		response = HTTParty.get('https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_GSPC.json?api_key=#{QUANDL_API_KEY}&start_date=2016-11-20', :headers =>{'Content-Type' => 'application/json'})
+		uri = "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_GSPC.json?api_key=#{QUANDL_API_KEY}&start_date=2016-11-20"
+		response = HTTParty.get(uri, :headers =>{'Content-Type' => 'application/json'})
 		body = JSON.parse(response.body)
 		dataset = body["dataset"]
 		d = DateTime.parse(dataset["refreshed_at"]).strftime("%m/%d/%Y %I:%M")
