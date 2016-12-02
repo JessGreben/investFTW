50759ae496612c7eea349c398359d4f885b5e0b4
Jessica Grebenschikov <Jessica@bright.md>
2016-12-02 14:53:32 -0800

    move api call from controller to stock model

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,13 +1,6 @@
 class InvestmentsController < ApplicationController
 	def new
-		key = ENV["QUANDL_API_KEY"]
-		uri = "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_GSPC.json?api_key=#{key}&start_date=2016-11-20"
-		response = HTTParty.get(uri, :headers =>{'Content-Type' => 'application/json'})
-		body = JSON.parse(response.body)
-		dataset = body["dataset"]
-		d = DateTime.parse(dataset["refreshed_at"]).strftime("%m/%d/%Y %I:%M")
-		@updatedAt = d
-		@yesterdayClose = dataset["data"][0][4]
+		@stock_activity = StockTradingActivity.new
 		render
 	end
 
